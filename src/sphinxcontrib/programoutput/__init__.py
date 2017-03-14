1c778c729cd46b1f5a4f3adfef5afea7d3fa7fbc
Jason Madden <jamadden@gmail.com>
2017-03-14 10:28:56 -0500

    Version specific qualifiers for what we test. Also compatibility with zest.releaser. Fixes #6

--- src/sphinxcontrib/programoutput/__init__.py
+++ src/sphinxcontrib/programoutput/__init__.py
@@ -47,7 +47,7 @@ from docutils.parsers import rst
 from docutils.parsers.rst.directives import flag, unchanged, nonnegative_int
 
 
-__version__ = '0.8'
+__version__ = '0.9.dev0'
 
 
 class program_output(nodes.Element):
