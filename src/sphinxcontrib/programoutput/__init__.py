93f620beb7dfae34dbe332cc8c5df080ea2e89e1
Jason Madden <jamadden@gmail.com>
2019-09-16 06:37:56 -0500

    Back to development: 0.16

--- src/sphinxcontrib/programoutput/__init__.py
+++ src/sphinxcontrib/programoutput/__init__.py
@@ -47,7 +47,7 @@ from docutils.parsers.rst.directives import flag, unchanged, nonnegative_int
 
 from sphinx.util import logging as sphinx_logging
 
-__version__ = '0.15'
+__version__ = '0.16.dev0'
 
 logger = sphinx_logging.getLogger('contrib.programoutput')
 
