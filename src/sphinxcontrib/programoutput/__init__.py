e1dc8d0a1c1738e4fa1984ecc800bef6c3e2983f
Jason Madden <jamadden@gmail.com>
2024-12-06 14:37:02 -0600

    Back to development: 0.19

--- src/sphinxcontrib/programoutput/__init__.py
+++ src/sphinxcontrib/programoutput/__init__.py
@@ -48,7 +48,7 @@ from docutils.statemachine import StringList
 
 from sphinx.util import logging as sphinx_logging
 
-__version__ = '0.18'
+__version__ = '0.19.dev0'
 
 logger = sphinx_logging.getLogger('contrib.programoutput')
 
